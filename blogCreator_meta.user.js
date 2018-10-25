244209b5b4f37dbfbf494fb10ae7cc89cf18531d
Jun Kim <jun.kim@cdk.com>
2018-10-25 08:19:21 -0700

    include downloadURL

--- blogCreator_meta.user.js
+++ blogCreator_meta.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Blog Creator
-// @version      0.1
+// @version      0.2
 // @author       jk
 // @include      http://nitra.wip*
 // @include      *?nextGen=true&pageName=Blog
 // @include      *Blog
+// @downloadURL  https://github.com/chongjk88/blogCreator/raw/master/blogCreator_meta.user.js
+// @require      https://github.com/chongjk88/blogCreator/raw/master/blogCreator.js
 // @grant        GM_openInTab
 // ==/UserScript==
